e62b159528f0578c4b5775e578a90f83fdb8462c
admin <admin@YD3-10>
2022-10-05 17:54:28 +0900

    수정

--- ZiPPY/front/src/components/property/ClusteringMap.vue
+++ ZiPPY/front/src/components/property/ClusteringMap.vue
@@ -5,38 +5,45 @@
 </template>
 
 <script>
-export default {
-  mounted() {
-    window.kakao && window.kakao.maps
-      ? this.initMap()
-      : this.addKakaoMapScript();
-  },
-  methods: {
-    addKakaoMapScript() {
-      const script = document.createElement("script");
-      /* global kakao */
-      script.onload = () => kakao.maps.load(this.initMap);
-      script.src =
-        "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=본인이 발급받은 appkey 등록";
-      document.head.appendChild(script);
+  export default {
+    mounted() {
+      window.kakao && window.kakao.maps ?
+        this.initMap() :
+        this.addKakaoMapScript();
     },
-    initMap() {
-      var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-      var options = {
-        //지도를 생성할 때 필요한 기본 옵션
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3 //지도의 레벨(확대, 축소 정도)
-      };
+    methods: {
+      addKakaoMapScript() {
+        const script = document.createElement("script");
+        /* global kakao */
+        script.onload = () => kakao.maps.load(this.initMap);
+        script.src =
+          "http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=본인이 발급받은 appkey 등록";
+        document.head.appendChild(script);
+      },
+      initMap() {
+        var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+        var options = {
+          center: new kakao.maps.LatLng(36.2683, 127.6358), // 지도의 중심좌표 
+          level: 14 // 지도의 확대 레벨 
+        };
 
-      var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+        var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+
+        var clusterer = new kakao.maps.MarkerClusterer({
+          map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체 
+          averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정 
+          minLevel: 10 // 클러스터 할 최소 지도 레벨 
+        });
+
+      }
+
     }
-  }
-};
+  };
 </script>
 
 <style>
-.map {
-  width: 100%;
-  height: 400px;
-}
+  .map {
+    width: 100%;
+    height: 400px;
+  }
 </style>
