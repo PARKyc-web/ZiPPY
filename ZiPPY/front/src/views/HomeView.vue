2d8bf23e3e25068ecb3d1387ac91e620c7c3a63c
admin <admin@YD3-10>
2022-10-05 17:34:02 +0900

    vue - kakao map

--- ZiPPY/front/src/views/HomeView.vue
+++ ZiPPY/front/src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
-  <hello-world />
+  <!-- <hello-world /> -->
+  <clustering-map/>
 </template>
 
 <script>
 import HelloWorld from "../components/HelloWorld";
+import ClusteringMap from "../components/property/ClusteringMap.vue";
 
 export default {
   name: "Home",
 
   components: {
     HelloWorld,
+    ClusteringMap,
   },
 };
 </script>
