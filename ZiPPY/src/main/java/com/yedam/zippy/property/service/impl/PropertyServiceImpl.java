006dac07c9393a8562d8ae0caef65544eb0d9bfc
admin <admin@YD3-10>
2022-10-12 17:03:59 +0900

    10-12

--- ZiPPY/src/main/java/com/yedam/zippy/property/service/impl/PropertyServiceImpl.java
+++ ZiPPY/src/main/java/com/yedam/zippy/property/service/impl/PropertyServiceImpl.java
@@ -11,39 +11,55 @@ import com.yedam.zippy.property.service.agentVO;
 import com.yedam.zippy.property.service.propertyVO;
 
 @Service
-public class PropertyServiceImpl implements PropertyService{
+public class PropertyServiceImpl implements PropertyService {
 
-	@Autowired
-	PropertyMapper mapper;
-	
-	@Override
-	public List<propertyVO> getAllPropertyList() {
-		return mapper.getAllPropertyList();
-	}
-	
-	@Override
-	public String getAgentEmail(int productId) {
-	  return mapper.getAgentEmail(productId);
-	}
-	
-	@Override
-	public List<propertyVO> gethouseDetail(int productId) {
-	  return mapper.gethouseDetail(productId);
-	}
-	
-	@Override
-	public List<propertyVO> getAgentProperties(String email){
-	  return mapper.getAgentProperties(email);
-	}
-	
-	@Override
-	public List<agentVO> getAgentProfile(String businessEmail){
-	  return mapper.getAgentProfile(businessEmail);
-	}
-	
-	@Override
-	public String getCompName(String businessEmail) {
-	  return mapper.getCompName(businessEmail);
-	}
-	
+  @Autowired
+  PropertyMapper mapper;
+
+  @Override
+  public List<propertyVO> getAllPropertyList() {
+    return mapper.getAllPropertyList();
+  }
+
+  @Override
+  public String getAgentEmail(int productId) {
+    return mapper.getAgentEmail(productId);
+  }
+
+  @Override
+  public List<propertyVO> gethouseDetail(int productId) {
+    return mapper.gethouseDetail(productId);
+  }
+
+  @Override
+  public List<propertyVO> getAgentProperties(String email) {
+    return mapper.getAgentProperties(email);
+  }
+
+  @Override
+  public List<agentVO> getAgentProfile(String email) {
+    return mapper.getAgentProfile(email);
+  }
+
+  @Override
+  public String getCompName(String email) {
+    return mapper.getCompName(email);
+  }
+  
+  @Override
+  public int insertHouseProduct(propertyVO vo) {
+    return mapper.insertHouseProduct(vo);
+  }
+
+  @Override
+  public void updateHouseProduct(propertyVO vo) {
+    mapper.updateHouseProduct(vo);
+  }
+
+  @Override
+  public void updateHouseDetail(propertyVO vo) {
+    mapper.updateHouseDetail(vo);
+  }
+  
+
 }
--- ZiPPY/src/main/java/com/yedam/zippy/property/service/propertyVO.java
+++ ZiPPY/src/main/java/com/yedam/zippy/property/service/propertyVO.java
@@ -28,5 +28,4 @@ public class propertyVO {
 	private String streetAddress;
 	
 	private String compName;
-	
 }
