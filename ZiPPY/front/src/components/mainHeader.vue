6a21973850b5b847d4f121ec4dcfc008f1a9fb45
PARKyc <erty1201@naver.com>
2022-10-14 17:10:18 +0900

    commit

--- ZiPPY/front/src/components/mainHeader.vue
+++ ZiPPY/front/src/components/mainHeader.vue
@@ -1,9 +1,9 @@
 <template>
     <header>
       <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <div class="container-xxl">
+        <div class="container-fluid">
           <router-link to="/home" class="navbar-brand">
-            <img alt="ZiPPY_logo" src="@/assets/zippy_logo.png" style="height:48px;" />
+            <img alt="ZiPPY_logo" src="@/assets/zippy_logo.png" style="height:36px;" />
           </router-link>
 
           <ul class="navbar-nav me-auto">
@@ -19,15 +19,19 @@
             <li class="nav-item">
               <router-link to="/used" class="nav-link">중고거래</router-link>
             </li>             
-		  </ul>          
-          <button v-if="this.$store.state.login_info != null"><img id="chatIcon" src="@/assets/chat.png"></button>  
-
+		  </ul>                   
           <div v-if="this.$store.state.login_info == null">
-            <router-link to="/login" class="nav-link"><img id="chatIcon" src="@/assets/signin.png"></router-link>
+            <router-link to="/login" class="nav-link"><img id="Icon" src="@/assets/signin.png"></router-link>
           </div> 
           
+          <button v-if="this.$store.state.login_info != null"><img id="Icon" src="@/assets/chat.png"></button>
+
           <div v-if="this.$store.state.login_info != null">
-            <router-link to="/logout" class="nav-link"><img id="chatIcon" src="@/assets/logout.png"></router-link>
+            <router-link to="/mypage" class="nav-link"><img id="Icon" src="@/assets/user.png"></router-link>
+          </div>
+
+          <div v-if="this.$store.state.login_info != null">
+            <router-link to="/logout" class="nav-link"><img id="Icon" src="@/assets/logout.png"></router-link>
           </div>
 		</div>
       </nav>
@@ -35,35 +39,41 @@
 </template>
 
 <script>
+
 export default{
 
 }
 </script>
 
 <style scoped>
+
 nav .navbar-nav a.router-link-active{
-  color: #42b983 !important;
+  color: #42b983 !important;  
 }
+
 nav .navbar-nav a.nav-link {
   font-size: larger;
   font-weight: bold;
+  text-decoration: none;
   /* color: #2c3e50 !important; */
 }
+
 ul {
   margin-top: 10px;
   vertical-align: middle;
 }
 
-#chatIcon{
+#Icon{
     height:38px;
     margin-right: 20px;
 }
 
-#chatIcon:hover {    
-    filter: opacity(0.5) drop-shadow(0 0 0 #42b983);    
+#Icon:hover {    
+    filter: opacity(0.3) drop-shadow(0 0 0 #42b983);    
 }
 
 header {
-  box-shadow: 0 4px 4px -4px black;
+  box-shadow: 0 4px 2px -4px black;
 }
+
 </style>
