100561a0fc078e56e103d6fc12144c3dee84ea12
admin <admin@YD3-10>
2022-10-18 09:12:48 +0900

    10-17 home

--- ZiPPY/front/src/views/property/PropertyMain.vue
+++ ZiPPY/front/src/views/property/PropertyMain.vue
@@ -12,12 +12,6 @@
     <aside>
       <div v-if="houseProducts.length != 0" v-for="item in houseProducts" @click="goHouseDetail(item.productId)">
         <v-card>
-          <!-- <div>매물번호 {{item.productId}}</div>
-          <div>{{item.houseName}}</div>
-          <div>{{item.saleType}} {{item.price}}</div>
-          <div>{{item.sigungu}}</div>
-          <div>{{item.areaExclusive}}m² {{item.floor}}층</div>
-          <div>{{item.detailContents}}</div> -->
           <table>
             <tr>
               <td style="width: 35%;">여기에 이미지</td>
@@ -216,6 +210,10 @@
           makeClusterer();
         }, 500);
 
+        if(markers.length == 894) {
+          clearInterval(makeClusterer);
+        }
+
         // 마커 클러스터러에 클릭이벤트를 등록합니다
         // 마커 클러스터러를 생성할 때 disableClickZoom을 true로 설정하지 않은 경우
         // 이벤트 헨들러로 cluster 객체가 넘어오지 않을 수도 있습니다
@@ -223,16 +221,25 @@
           console.log(cluster.getCenter());
 
           // 현재 지도 레벨에서 1레벨 확대한 레벨
-          var level = map.getLevel() - 1;
+          var level = map.getLevel() - 2;
 
           // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
           map.setLevel(level, {
             anchor: cluster.getCenter()
           });
 
+          // console.log("click: ", initThis.sigungu);
+          // initThis.getPropertyList(initThis.sigungu);
+
+        });
+
+        function clickForList() {
           console.log("click: ", initThis.sigungu);
           initThis.getPropertyList(initThis.sigungu);
-        });
+        }
+        // setInterval(function () {
+        //   clickForList();
+        // }, 1000);
 
       },
       getPropertyList(sigungu) {
