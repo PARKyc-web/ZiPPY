656882a3b51f89b31a0e0b12d06b9c979d7185a1
admin <admin@YD3-10>
2022-10-21 10:01:35 +0900

    중고거래 현 위치 출력

--- ZiPPY/front/src/views/used/usedMain.vue
+++ ZiPPY/front/src/views/used/usedMain.vue
@@ -6,11 +6,20 @@
         <div class="used-main-title">
           <h3>판매중인 중고제품</h3>
         </div>
+
         <div id="used-add-drop-search">
           <button id="used-addr">
             <i class="fa-solid fa-location-dot fa-2x"></i>
           </button>
-          <div><h2>{{this.categoryVal}}</h2></div>
+          <div class="map_wrap">
+            <div id="map"></div>
+            <div class="hAddr">
+              <span id="centerAddr"></span>
+            </div>
+          </div>
+          <div>
+            <h2>{{this.categoryVal}}</h2>
+          </div>
           <div>
             <div class="search">
               <input type="text" placeholder="검색어 입력" id="used-main-search-input" v-model="word"
@@ -97,7 +106,13 @@
       isChecked: '',
       location: '',
       pageCount: 1,
-      dropValue: ''
+      dropValue: '',
+      
+      // 주소 출력에 필요한 데이터들입니다.
+      map: null,
+      markers: [],
+      latitude: 0,
+      longitude: 0
     }),
     filters: {
       comma(val) {
@@ -134,6 +149,30 @@
         checked: "",
         dropbox: "",
         pageNum: this.page
+      })
+
+      if (!("geolocation" in navigator)) {
+        return;
+      }
+
+      // get position
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.latitude = pos.coords.latitude;
+        this.longitude = pos.coords.longitude;
+
+        if (window.kakao && window.kakao.maps) {
+
+          this.initMap();
+
+        } else {
+          const script = document.createElement("script");
+          /* global kakao */
+          script.onload = () => kakao.maps.load(this.initMap);
+          document.head.appendChild(script);
+        }
+
+      }, err => {
+        alert(err.message);
       })
     },
     methods: {
@@ -214,6 +253,70 @@
         }).catch(err => {
           console.log(err)
         })
+      },
+      initMap() {
+        const container = document.getElementById("map");
+        const options = {
+          center: new kakao.maps.LatLng(33.450701, 126.570667),
+          level: 5,
+        };
+        this.map = new kakao.maps.Map(container, options);
+        this.displayMarker([
+          [this.latitude, this.longitude]
+        ]);
+      },
+
+      displayMarker(markerPositions) {
+        if (this.markers.length > 0) {
+          this.markers.forEach((marker) => marker.setMap(null));
+        }
+
+        const positions = markerPositions.map(
+          (position) => new kakao.maps.LatLng(...position)
+        );
+
+        if (positions.length > 0) {
+          this.markers = positions.map(
+            (position) =>
+            new kakao.maps.Marker({
+              map: this.map,
+              position,
+            })
+          );
+
+          const bounds = positions.reduce(
+            (bounds, latlng) => bounds.extend(latlng),
+            new kakao.maps.LatLngBounds()
+          );
+
+          this.map.setBounds(bounds);
+
+          // 주소-좌표 변환 객체를 생성합니다
+          var geocoder = new kakao.maps.services.Geocoder();
+
+          // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
+          searchAddrFromCoords(this.map.getCenter(), displayCenterInfo);
+
+          function searchAddrFromCoords(coords, callback) {
+            // 좌표로 행정동 주소 정보를 요청합니다
+            geocoder.coord2RegionCode(coords.getLng(), coords.getLat(), callback);
+          }
+
+          // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
+          function displayCenterInfo(result, status) {
+            if (status === kakao.maps.services.Status.OK) {
+              var infoDiv = document.getElementById('centerAddr');
+
+              for (var i = 0; i < result.length; i++) {
+                // 행정동의 region_type 값은 'H' 이므로
+                if (result[i].region_type === 'H') {
+                  infoDiv.innerHTML = result[i].address_name;
+                  break;
+                }
+              }
+            }
+          }
+        }
       }
     }
   }
@@ -519,5 +622,33 @@
     border-color: #B3E3C3;
     font-size: 13px;
     text-align: center;
+  }
+
+  .map_wrap {
+    position: relative;
+    width: 100%;
+    height: 350px;
+  }
+
+/* 주소 폰트는 여기서 조절하시면 됩니다. */
+  .hAddr {
+    position: absolute;
+    left: 10px;
+    top: 10px;
+    border-radius: 2px;
+    z-index: 1;
+  }
+
+  #centerAddr {
+    display: block;
+    margin-top: 2px;
+    font-weight: normal;
+  }
+
+  .bAddr {
+    padding: 5px;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
   }
 </style>
