8afb14740662b99fafa8499cd4e6fd723c026dd2
EJU <wjddnd1745@naver.com>
2022-10-24 19:03:54 +0900

    EJU 10.24 19:03

--- ZiPPY/front/src/components/used/navBar.vue
+++ ZiPPY/front/src/components/used/navBar.vue
@@ -1,15 +1,39 @@
+<!-- <template>
+  <v-app-bar color="#fff">
+    <a @click="pushCategory($event)" class="nav-link active" aria-current="page" href="#">전체</a>
+    <a @click="pushCategory($event)" class="nav-link" href="#">침실</a>
+    <a @click="pushCategory($event)" class="nav-link" href="#">수납</a>
+    <a @click="pushCategory($event)" class="nav-link" href="#">주방</a>
+    <a @click="pushCategory($event)" class="nav-link" href="#">욕실</a>
+    <a @click="pushCategory($event)" class="nav-link" href="#">서재</a>
+    <a @click="pushCategory($event)" class="nav-link" href="#">다용도실</a>
+  </v-app-bar>
+</template> -->
 <template>
-  <div class="nav-scroller bg-body shadow-sm">
-    <nav class="nav" aria-label="Secondary navigation">
-      <a @click="pushCategory($event)" class="nav-link active" aria-current="page" href="#">전체</a>
-      <a @click="pushCategory($event)" class="nav-link" href="#">침실</a>
-      <a @click="pushCategory($event)" class="nav-link" href="#">수납</a>
-      <a @click="pushCategory($event)" class="nav-link" href="#">주방</a>
-      <a @click="pushCategory($event)" class="nav-link" href="#">욕실</a>
-      <a @click="pushCategory($event)" class="nav-link" href="#">서재</a>
-      <a @click="pushCategory($event)" class="nav-link" href="#">다용도실</a>
-    </nav>
-  </div>
+  <nav class="py-2 border-bottom">
+    <div class="container d-flex flex-wrap">
+      <ul class="nav me-auto">
+        <li class="nav-item">
+          <a @click="pushCategory($event)" href="#" class="nav-link link-dark px-2 active" aria-current="page">침실</a>
+        </li>
+        <li class="nav-item">
+          <a @click="pushCategory($event)" href="#" class="nav-link link-dark px-2">수납</a>
+        </li>
+        <li class="nav-item">
+          <a @click="pushCategory($event)" href="#" class="nav-link link-dark px-2">주방</a>
+        </li>
+        <li class="nav-item">
+          <a @click="pushCategory($event)" href="#" class="nav-link link-dark px-2">욕실</a>
+        </li>
+        <li class="nav-item">
+          <a @click="pushCategory($event)" href="#" class="nav-link link-dark px-2">서재</a>
+        </li>
+        <li class="nav-item">
+          <a @click="pushCategory($event)" href="#" class="nav-link link-dark px-2">다용도실</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -38,7 +62,12 @@
     width: 100%;
   }
 
-  .nav a {
-    color: black;
+  .nav-item{
+    margin-right: 10px;
+  }
+  .nav-item:hover{
+    background-color: #b3e3c3;
+    border-radius: 20%;
+    
   }
 </style>
