e09e6465745750f8437072b74abe05defae5b0c0
PARKyc <erty1201@naver.com>
2022-10-25 16:37:54 +0900

    123

--- ZiPPY/front/vue.config.js
+++ ZiPPY/front/vue.config.js
@@ -2,4 +2,12 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
   lintOnSave: false,
+  devServer: {
+    proxy: {
+      '/zippy': { 
+        target: 'http://localhost:90',
+        changeOrigin: true,
+      }
+    }
+  }
 });
