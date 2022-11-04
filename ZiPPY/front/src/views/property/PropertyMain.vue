c6339ed45c0ec3e2e1f39c1234b7e53f5fcea28b
PARKyc <48409531+PARKyc-web@users.noreply.github.com>
2022-11-04 10:35:42 +0900

    Update PropertyMain.vue

--- ZiPPY/front/src/views/property/PropertyMain.vue
+++ ZiPPY/front/src/views/property/PropertyMain.vue
@@ -150,7 +150,7 @@
       },
       initMap() {
         var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-        const MIN_LEVEL = 5;
+        const MIN_LEVEL = 4;
         var options = {
           //지도를 생성할 때 필요한 기본 옵션
           center: new kakao.maps.LatLng(35.8690419, 128.593347), //지도의 중심좌표.
