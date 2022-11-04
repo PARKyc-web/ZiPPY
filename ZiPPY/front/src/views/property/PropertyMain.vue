19919c5510dab9226c85b19ae0edabdb1cd4dfb4
youngrank <66213937+youngrank@users.noreply.github.com>
2022-11-04 09:32:23 +0900

    지도 레벨 변경

--- ZiPPY/front/src/views/property/PropertyMain.vue
+++ ZiPPY/front/src/views/property/PropertyMain.vue
@@ -150,11 +150,11 @@
       },
       initMap() {
         var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-        const MIN_LEVEL = 6;
+        const MIN_LEVEL = 5;
         var options = {
           //지도를 생성할 때 필요한 기본 옵션
           center: new kakao.maps.LatLng(35.8690419, 128.593347), //지도의 중심좌표.
-          level: 5 //지도의 레벨(확대, 축소 정도)
+          level: 7 //지도의 레벨(확대, 축소 정도)
         };
 
         this.map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
